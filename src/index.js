3a61586c762da438155814f8c5864c54b145c96e
sturdy-robot <pedrenriquegg@hotmail.com>
2020-04-08 17:12:55 -0300

    feat(drawer): add drawer and first page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Drawer from './Drawer'
 import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Drawer />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
